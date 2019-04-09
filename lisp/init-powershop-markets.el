a770579d0476c052dcfd09a8ecc3d25e1c9df441
Vincent Boucheny <vincent.boucheny@fluxfederation.com>
2019-04-09 14:38:25 +1200

    Update rspec commands to keep up with current version

--- lisp/init-powershop-markets.el
+++ lisp/init-powershop-markets.el
@@ -1,10 +1,10 @@
 ;;; Setup powershop markets
-(defconst powershop-markets '("nz" "au" "uk"))
+(defconst powershop-markets '("psnz" "psau" "psuk" "merx"))
 (defvar powershop-market-history nil)
 (defun powershop-read-market ()
   (completing-read "Market: " powershop-markets nil t
                    nil
                    'powershop-market-history
-                   (or (car powershop-market-history) "au")
+                   (or (car powershop-market-history) "psau")
                    ))
 (provide 'init-powershop-markets)
--- lisp/init-rspec-mode.el
+++ lisp/init-rspec-mode.el
@@ -4,10 +4,10 @@
 (require 'rspec-mode)
 (require 'init-powershop-markets)
 
-(setq rspec-spec-command " bundle exec spring rspec")
+(setq rspec-spec-command " bundle exec rspec")
 (setq rspec-use-bundler-when-possible nil)
 (setq rspec-use-spring-when-possible nil)
-(setq rspec-primary-source-dirs '("app" "lib" "core" "psnz" "psau" "psuk"))
+(setq rspec-primary-source-dirs '("app" "lib" "core" "nz" "au" "uk"))
 (setq rspec-command-options "--color --format documentation")
 
 (define-key rspec-verifiable-mode-keymap (kbd "v") 'powershop-rspec-verify)
@@ -42,7 +42,12 @@
 
 (defun powershop-override-rspec-function (rspec-function)
   (let ((rspec-spec-command
-         (format "PS_MARKET=%s bundle exec spring rspec" market)))
+         (format "%s %s"
+                 (cond ((equal market "psnz") "RETAILER=psnz COUNTRY=nz RETAIL_BRAND=powershop")
+                       ((equal market "psau") "RETAILER=psau COUNTRY=au RETAIL_BRAND=powershop")
+                       ((equal market "psuk") "RETAILER=psuk COUNTRY=uk RETAIL_BRAND=powershop")
+                       ((equal market "merx") "RETAILER=merx COUNTRY=nz RETAIL_BRAND=meridian" ))
+                 rspec-spec-command)))
     (funcall rspec-function)))
 
 (add-hook 'after-init-hook 'inf-ruby-switch-setup)
